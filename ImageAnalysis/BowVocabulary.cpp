f569d1984c544a0e999f09894c605be6a4ac9adf
minhnt <minhnguyen0190@gmail.com>
2015-10-30 09:43:40 +0700

    Train SVM with 6 classes

--- ImageAnalysis/BowVocabulary.cpp
+++ ImageAnalysis/BowVocabulary.cpp
@@ -173,8 +173,14 @@ bool BowVocabulary::computeTrainingData(TrainingData& trainingDataOut, const str
 						trainLabels.push_back(1);
 					}else if(i<81){
 						trainLabels.push_back(2);
-					}else{
+					}else if(i<121){
 						trainLabels.push_back(3);
+					}else if(i<161){
+						trainLabels.push_back(4);
+					}else if(i<201){
+						trainLabels.push_back(5);
+					}else{
+						trainLabels.push_back(6);
 					}
 					
 				}
